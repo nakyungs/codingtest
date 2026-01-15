7966d84572602749424adf19d224096736f293d1
nakyungs <122212024+nakyungs@users.noreply.github.com>
2026-01-15 14:25:36 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+data = int(input())
+
+if 90 <= data <= 100:
+    print('A')
+elif 80 <= data < 90:
+    print('B')
+elif 70 <= data < 80:
+    print('C')
+elif 60 <= data < 70:
+    print('D')
+else:
+    print('F')
