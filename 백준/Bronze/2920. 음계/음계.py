895ac316a9b1094bef0ad6f761b731e20558220f
nakyungs <122212024+nakyungs@users.noreply.github.com>
2026-01-15 10:26:14 +0900

    [Bronze II] Title: 음계, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.py
@@ -0,0 +1,8 @@
+data = list(map(int, input().split()))
+
+if data == sorted(data):
+    print('ascending')
+elif data == sorted(data, reverse=True):
+    print('descending')
+else:
+    print('mixed')
